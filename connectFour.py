f0a7c9b78f57b10f7cab333392e0951abaeff48c
Akins <Akins@Jamess-Air.localdomain>
2019-10-28 18:26:45 -0700

    first commit

---
+++ connectFour.py
@@ -0,0 +1,52 @@
+class Player:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def name(self):
+        name = input("What is the players name? ")
+        return player
+
+    def color(self):
+        color = input("What is the players color? ")
+        return color
+    
+class Game:
+    def __init__(
+        self,
+        board,
+        row1 = [0, 0, 0, 0, 0, 0],
+        row2 = [0, 0, 0, 0, 0, 0],
+        row3 = [0, 0, 0, 0, 0, 0],
+        row4 = [0, 0, 0, 0, 0, 0],
+        row5 = [0, 0, 0, 0, 0, 0],
+        row6 = [0, 0, 0, 0, 0, 0],
+        row7 = [0, 0, 0, 0, 0, 0],
+        )
+
+    def board(self):
+    
+
+
+    def getHeight(self):
+        #find the number of 0 left in list of six to determine. 
+    def move(self):
+        #pick a row to place color. Color is placed at the last availible 0
+
+    #def calcWinner():
+
+    #def isFull():
+
+    #def gameOverCheck():
+
+def main():
+    theGame = Game()
+
+    player1 = Player.name()
+    player1Color = Player.color()
+
+    player2 = Player.name()
+    player2Color = Player.color()
+
+
+
